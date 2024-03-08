ca849fc9bcd4092f0f9845660fb3d85bf9221f8e
TrojanBlazing <145752343+TrojanBlazing@users.noreply.github.com>
2024-03-08 18:57:52 +0530

    HalfofSegment1

---
+++ Assets/Scenes/VinilScripts/Dialouge/InterDisable.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class InterDisable : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject door;
+
+    [SerializeField]
+    private GameObject Act;
+
+    [SerializeField]
+    private AudioSource am;
+
+    [SerializeField]
+    private GameObject text;
+    public string dialouge = "Yea hes asleep";
+
+    public float timer = 3f;
+
+    private void Start()
+    {
+        text.SetActive(false);
+        door.gameObject.SetActive(false);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+
+            text.SetActive(true);
+            am.Play();
+            door.gameObject.SetActive(true);
+
+            StartCoroutine(TextDisable());
+
+        }
+    }
+    IEnumerator TextDisable()
+    {
+        yield return new WaitForSeconds(timer);
+        text.SetActive(false);
+        Destroy(Act);
+    }
+
+}
