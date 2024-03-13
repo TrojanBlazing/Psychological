a4a79942366850b818b576d6c2d6310c3c8af1a7
TrojanBlazing <145752343+TrojanBlazing@users.noreply.github.com>
2024-03-13 12:07:17 +0530

    HouseFbx

--- Assets/VinilScripts/CameraZoomout/CameraZoomOut.cs
+++ Assets/VinilScripts/CameraZoomout/CameraZoomOut.cs
@@ -2,12 +2,14 @@ using UnityEngine;
 
 public class CamerasZoomOut : MonoBehaviour
 {
-    public Transform cubeToZoomOutFrom;   
-    public float zoomSpeed = 2.0f;        
-    public float minDistance = 2.0f;      
-    public float maxDistance = 10.0f;     
-    public float stopDistance = 5.0f;  
-    
+    public Transform cubeToZoomOutFrom;
+    public float zoomSpeed = 2.0f;
+    public float minDistance = 2.0f;
+    public float maxDistance = 10.0f;
+    public float stopDistance = 5.0f;
+
+
+
 
 
     private Vector3 initialPosition;
@@ -15,6 +17,7 @@ public class CamerasZoomOut : MonoBehaviour
 
     void Start()
     {
+
         cam = GetComponent<Camera>();
         initialPosition = transform.position;
     }
@@ -23,20 +26,21 @@ public class CamerasZoomOut : MonoBehaviour
     {
         float distance = Vector3.Distance(transform.position, cubeToZoomOutFrom.position);
 
-        
+
         float desiredDistance = Mathf.Lerp(minDistance, maxDistance, distance / stopDistance);
 
-        
+
         Vector3 directionToCube = (cubeToZoomOutFrom.position - transform.position).normalized;
 
-        
+
         Vector3 desiredPosition = cubeToZoomOutFrom.position - directionToCube * desiredDistance;
 
-        
+
         if (distance > stopDistance)
         {
-           
+
             transform.position = Vector3.Lerp(transform.position, desiredPosition, Time.deltaTime * zoomSpeed);
         }
+
     }
 }
